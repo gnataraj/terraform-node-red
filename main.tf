bcb63c8edc0c012be664c32a38e4247c4c568f73
Nataraj G <mail2natti@gmail.com>
2021-11-19 18:55:04 +0530

    output added

--- main.tf
+++ main.tf
@@ -7,17 +7,29 @@ terraform {
   }
 }
 
-provider "docker" {} 
+provider "docker" {}
 
- resource "docker_image" "nodered-image" {
-   name = "nodered/node-red:latest"  
- }
+resource "docker_image" "nodered-image" {
+  name = "nodered/node-red:latest"
+}
 
- resource "docker_container" "nodered-container"{
-     name ="nodered"
-     image = docker_image.nodered-image.latest
-     ports {
-         internal = 1880
-         external = 1880
-     }
- }
+resource "docker_container" "nodered-container" {
+  name  = "nodered"
+  image = docker_image.nodered-image.latest
+  ports {
+    internal = 1880
+    external = 1880
+  }
+}
+
+output "container-name" {
+  value       = docker_container.nodered-container.name
+  description = "name of the docker container"
+
+}
+
+output "ip-address" {
+  value       = docker_container.nodered-container.ip_address
+  description = "ip address of the container"
+
+}
