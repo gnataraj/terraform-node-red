f56c54668e0c8271c310cf7e77ca8d285caaf1d6
Nataraj G <mail2natti@gmail.com>
2021-12-03 14:24:24 +0530

    two repeating containers

--- main.tf
+++ main.tf
@@ -9,27 +9,62 @@ terraform {
 
 provider "docker" {}
 
+resource "random_string" "random1" {
+  length           = 4
+  special          = false
+  upper=false
+  }
+
+resource "random_string" "random2" {
+  length           = 4
+  special          = false
+  upper = false
+  }
+
+
 resource "docker_image" "nodered-image" {
   name = "nodered/node-red:latest"
 }
 
-resource "docker_container" "nodered-container" {
-  name  = "nodered"
+resource "docker_container" "nodered-container1" {
+  name  = join("-", ["nodered", random_string.random1.result])
   image = docker_image.nodered-image.latest
   ports {
     internal = 1880
-    external = 1880
+    //external = 1880
   }
 }
 
-output "container-name" {
-  value       = docker_container.nodered-container.name
-  description = "name of the docker container"
+resource "docker_container" "nodered-container2" {
+  name  = join("-", ["nodered", random_string.random2.result])
+  image = docker_image.nodered-image.latest
+  ports {
+    internal = 1880
+    //external = 1880
+  }
+}
+
+
+
+ output "container-name1" {
+   value       = docker_container.nodered-container1.name
+   description = "name of the docker container1"
+
+ }
+
+ output "container-name2" {
+   value       = docker_container.nodered-container2.name
+   description = "name of the docker container1"
+
+ }
+output "ip-address1" {
+  value       = join(":", [docker_container.nodered-container1.ip_address, docker_container.nodered-container1.ports[0].external])
+  description = "ip address of the container"
 
 }
 
-output "ip-address" {
-  value       = join(":", [docker_container.nodered-container.ip_address, docker_container.nodered-container.ports[0].external])
+output "ip-address2" {
+  value       = join(":", [docker_container.nodered-container2.ip_address, docker_container.nodered-container2.ports[0].external])
   description = "ip address of the container"
 
 }
